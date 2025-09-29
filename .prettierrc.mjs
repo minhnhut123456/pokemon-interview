8049a0be8e5e2aa89be0a2b71973d94a2741c873
minhnhut123456 <83867213+minhnhut123456@users.noreply.github.com>
2025-09-30 01:53:05 +0700

    Create .prettierrc.mjs

---
+++ .prettierrc.mjs
@@ -0,0 +1,12 @@
+const config = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  insertFinalNewline: true,
+  printWidth: 90,
+};
+
+export default config;
